68322f79d5cad43d749f9f04fdd0488e8a8075ee
Edris Ríos Morales <44209633+Edrisrm@users.noreply.github.com>
2024-07-25 10:44:31 -0600

    change manifest and index

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar.jsx';
 import Home from './components/home.jsx';
@@ -6,23 +6,37 @@ import Footer from './components/layout/Footer.jsx';
 import Pokedex from './components/Pokedex.jsx';
 import DetailPokemon from './components/PokemonDetail/DetailPokemon.jsx';
 import PokeQuiz from './components/games/PokeQuiz.jsx';
+
 function App() {
+  useEffect(() => {
+    document.title = "Treeckopedia";
+
+    const metaDescription = document.querySelector('meta[name="description"]');
+    if (metaDescription) {
+      metaDescription.setAttribute("content", "Search for any Pokémon you want and know its main data, such as Skills, types and their habits.");
+    } else {
+      const meta = document.createElement('meta');
+      meta.name = "description";
+      meta.content = "Search for any Pokémon you want and know its main data, such as Skills, types and their habits.";
+      document.head.appendChild(meta);
+    }
+  }, []);
+
   return (
     <>
-    <Router >
-    <Navbar/>
-      <div className='bg-[#000000] bg-[radial-gradient(#ffffff33_1px,#00091d_1px)] bg-[size:20px_20px] min-h-screen '>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path='/pokedex' element={<Pokedex />} />
-          <Route path='/pokemon/:name' element={<DetailPokemon/>} />
-          <Route path='/minigames' element={<PokeQuiz/>} />
-        </Routes>
-      </div>
-    <Footer/>
-    </Router>
+      <Router>
+        <Navbar />
+        <div className='bg-[#000000] bg-[radial-gradient(#ffffff33_1px,#00091d_1px)] bg-[size:20px_20px] min-h-screen '>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path='/pokedex' element={<Pokedex />} />
+            <Route path='/pokemon/:name' element={<DetailPokemon />} />
+            <Route path='/minigames' element={<PokeQuiz />} />
+          </Routes>
+        </div>
+        <Footer />
+      </Router>
     </>
-    
   );
 }
 
